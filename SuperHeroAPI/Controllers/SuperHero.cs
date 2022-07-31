c595e1a7a1bd44cb3f1280c3bd55b27f23d9a482
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-07-30 21:33:32 -0400

    added Post and Get by Id method

--- SuperHeroAPI/Controllers/SuperHero.cs
+++ SuperHeroAPI/Controllers/SuperHero.cs
@@ -9,13 +9,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace SuperHeroAPI.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        [HttpGet]
-        // to see the sample in swagger, you need an ActionResult NOT the interface
-        public async Task<ActionResult<List<SuperHero>>> Get()
-        {
-            var heroes = new List<SuperHero>
+        private static List<SuperHero> heroes = new List<SuperHero>
             {
                 new SuperHero {
                     Id = 1,
@@ -23,9 +20,38 @@ namespace SuperHeroAPI.Controllers
                     FirstName = "Peter",
                     LastName = "Parker",
                     Place = "New York City"
-                }
+                },
+                 new SuperHero {
+                    Id = 2,
+                    Name = "Iron Man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"
+                },
             };
 
+         
+        [HttpGet]
+        // to see the sample in swagger, you need an ActionResult NOT the interface
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+        // to see the sample in swagger, you need an ActionResult NOT the interface
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found.");
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
             return Ok(heroes);
         }
     }
