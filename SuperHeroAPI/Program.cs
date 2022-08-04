cd6cb24fc5fa7b55dca3d949d241147d6da5b4de
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-08-04 19:44:57 -0400

    having trouble with connection string, will research postgres vs sqlexpress

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Registerted Data Context here
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
